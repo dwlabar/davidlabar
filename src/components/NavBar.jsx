148c138a6f02bd528d918b7c4f311842c95c1197
LaBar <dwlabar@gmail.com>
2025-02-25 11:27:28 -0500

    - added active state to nav-bar links

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,16 +1,18 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 import "../styles/components/_nav-bar.scss";
 
 const Nav = () => {
+  const location = useLocation();
+
   return (
     <nav className="nav-bar">
       <ul>
-        <li><Link to="/">Home</Link></li>
-        <li><Link to="/about">About</Link></li>
-        <li><Link to="/contact">Contact</Link></li>
-        <li><Link to="/svg-examples">SVG Examples</Link></li>
-        <li><Link to="/preloader-test">Preloader Test</Link></li>
+        <li className={location.pathname === "/" ? "active" : ""}><Link to="/">Home</Link></li>
+        <li className={location.pathname === "/about" ? "active" : ""}><Link to="/about">About</Link></li>
+        <li className={location.pathname === "/contact" ? "active" : ""}><Link to="/contact">Contact</Link></li>
+        <li className={location.pathname === "/svg-examples" ? "active" : ""}><Link to="/svg-examples">SVG Examples</Link></li>
+        <li className={location.pathname === "/preloader-test" ? "active" : ""}><Link to="/preloader-test">Preloader Test</Link></li>
       </ul>
     </nav>
   );
