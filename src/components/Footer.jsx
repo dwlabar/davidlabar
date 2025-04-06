248ab90892a49e45d0c80edb4ca86c11cce69528
LaBar <dwlabar@gmail.com>
2025-04-05 20:07:44 -0400

    remove container from footer and adjust padding

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,9 +5,7 @@ import "../styles/components/_footer.scss";
 const Footer = () => {
   return (
     <footer className="footer">
-      <Container>
-        <p className="footer__text"><strong>&copy;{new Date().getFullYear()} David LaBar Studios</strong> &nbsp; / &nbsp; All rights reserved.</p>
-      </Container>
+      <p className="footer__text"><strong>&copy;{new Date().getFullYear()} David LaBar Studios</strong> &nbsp; / &nbsp; All rights reserved.</p>
     </footer>
   );
 };
