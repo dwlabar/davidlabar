b74d1f4a14d7f7643de954384e660e70217a2baa
LaBar <dwlabar@gmail.com>
2026-02-11 15:12:11 -0500

    added autocomplete for email

--- src/components/FormContact.jsx
+++ src/components/FormContact.jsx
@@ -48,6 +48,7 @@ const ContactForm = () => {
         id="email"
         type="email"
         name="email"
+        autoComplete="email"
         value={email}
         onChange={(e) => setEmail(e.target.value)}
         required
