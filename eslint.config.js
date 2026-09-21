197dc1631c9803d7ba32851e62551a57cf9c69d5
LaBar <dwlabar@gmail.com>
2026-09-21 18:17:38 -0400

    chore: update ESLint tooling

--- eslint.config.js
+++ eslint.config.js
@@ -27,7 +27,8 @@ export default [
       ...js.configs.recommended.rules,
       ...react.configs.recommended.rules,
       ...react.configs['jsx-runtime'].rules,
-      ...reactHooks.configs.recommended.rules,
+      'react-hooks/rules-of-hooks': 'error',
+      'react-hooks/exhaustive-deps': 'warn',
       'react/jsx-no-target-blank': 'off',
       'react-refresh/only-export-components': [
         'warn',
